a8d623e4550944c0f826cc3fd992faf98f9bc361
baixiaodong <baixiaodong@lingchuang-dl.com>
2026-03-24 11:47:09 +0800

    i

--- main.cpp
+++ main.cpp
@@ -3,4 +3,6 @@
 // #include "../../WouoUI-128_64/WouoUI-128_64.ino"
 // #include "../../WouoUI-128_128/WouoUI-128_128.ino"
 // #include "../../WouoUI-Lite_General/WouoUI-Lite_General.ino"
-#include "../../WouoUI-wave/WouoUI-wave.ino"
+#include "WouoUI-Demo/WouoUI-wave.cpp"
+
+// export DISPLAY=:0
